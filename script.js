b72cdb42fb6cb7fbfacbe6014a11cfd9c1b8f6e6
Aakarshan Jorge <aaka022009@gmail.com>
2025-04-15 12:45:22 +0200

    Fixed an issue where the wrong text would be displayed

--- script.js
+++ script.js
@@ -3,15 +3,23 @@ let cashAmount = document.querySelector(".cash");
 let upgrades = document.querySelector(".upgrade");
 let cookieCount = 0;
 let cashCount = 0;
-let shop = document.querySelector(".shop")
+let shop = document.querySelector(".shop");
 let closeButton = document.querySelector(".close-button");
 let doubleCookies = false;
+let doubleCookiesBought = false;
 let fourCookies = false;
+let fourCookiesBought = false;
 let eightCookies = false;
+let eightCookiesBought = false;
 let twentyCookies = false;
+let twentyCookiesBought = false;
 let thousandCookies = false;
+let thousandCookiesBought = false;
+let twoTimesCash = false;
 let purchaseError = document.querySelector(".purchase-error");
 let alreadyOwn = document.querySelector(".already-own");
+let bonus = document.querySelector(".bonus");
+let currentCashAmount = document.querySelector(".cash-amount");
 
 function cookieClick(){
     if(!doubleCookies && !fourCookies && !eightCookies && !twentyCookies && !thousandCookies){
@@ -41,25 +49,35 @@ function cookieClick(){
 }
 
 function sellCookies(){
-    cashCount = cashCount + cookieCount;
-    cashAmount.innerHTML = cashCount;
-    cookieCount = 0;
-    cookieAmount.innerHTML = cookieCount;
+    if(!twoTimesCash){
+        cashCount = cashCount + cookieCount;
+        cashAmount.innerHTML = cashCount;
+        cookieCount = 0;
+        cookieAmount.innerHTML = cookieCount;
+    } 
+    else{
+        cashCount = cashCount + cookieCount * 2;
+        cashAmount.innerHTML = cashCount;
+        cookieCount = 0;
+        cookieAmount.innerHTML = cookieCount;
+    }
 }
 
 function doubleCookiesPurchase(){
     if(cashCount >= 50 && !doubleCookies && !fourCookies && !eightCookies && !twentyCookies && !thousandCookies){
         doubleCookies = true;
+        doubleCookiesBought = true
         cashCount = cashCount - 50;
         cashAmount.innerHTML = cashCount;
         upgrades.innerHTML = "2x cookies";
         purchaseError.style.display = "none";
     }
-    else if(doubleCookies){
+    else if(doubleCookies || doubleCookiesBought){
         alreadyOwn.style.display = "block";
     }
     else{
         purchaseError.style.display = "block";
+        currentCashAmount.innerHTML = cashCount;
     }
 }
 
@@ -67,16 +85,18 @@ function quadCookiesPurchase(){
     if(cashCount >= 100 && !fourCookies && doubleCookies){
         fourCookies = true;
         doubleCookies = false;
+        fourCookiesBought = true;
         cashCount = cashCount - 100;
         cashAmount.innerHTML = cashCount;
         upgrades.innerHTML = "4x cookies";
         purchaseError.style.display = "none";
     }
-    else if (fourCookies){
+    else if (fourCookies || fourCookiesBought){
         alreadyOwn.style.display = "block";
     }
     else{
         purchaseError.style.display = "block";
+        currentCashAmount.innerHTML = cashCount;
     }
 }
 
@@ -84,16 +104,18 @@ function eightCookiesPurchase(){
     if(cashCount >= 500 && !eightCookies && fourCookies){
         eightCookies = true;
         fourCookies = false;
+        eightCookiesBought = true;
         cashCount = cashCount - 500;
         cashAmount.innerHTML = cashCount;
         upgrades.innerHTML = "8x cookies"
         purchaseError.style.display = "none";
     }
-    else if(eightCookies){
+    else if(eightCookies || eightCookiesBought){
         alreadyOwn.style.display = "block";
     }
     else{
         purchaseError.style.display = "block";
+        currentCashAmount.innerHTML = cashCount;
     }
 }
 
@@ -101,16 +123,18 @@ function twentyCookiesPurchase(){
     if(cashCount >= 1000 && !twentyCookies && eightCookies){
         twentyCookies = true;
         eightCookies = false;
+        twentyCookiesBought = true;
         cashCount = cashCount - 1000;
         cashAmount.innerHTML = cashCount;
         upgrades.innerHTML = "20x cookies"
         purchaseError.style.display = "none";
     }
-    else if (twentyCookies){
+    else if (twentyCookies || twentyCookiesBought){
         alreadyOwn.style.display ="block";
     }
     else{
         purchaseError.style.display = "block";
+        currentCashAmount.innerHTML = cashCount;
     }
 }
 
@@ -123,11 +147,29 @@ function thousandCookiesPurchase(){
         upgrades.innerHTML = "1000x cookies";
         purchaseError.style.display = "none";
     }
-    else if(thousandCookies){
+    else if(thousandCookies || thousandCookiesBought){
         alreadyOwn.style.display = "block";
     }
     else{
         purchaseError.style.display = "block";
+        currentCashAmount.innerHTML = cashCount;
+    }
+}
+
+function twoTimes(){
+    if(cashCount >= 5000 && !twoTimesCash){
+        twoTimesCash = true;
+        bonus.innerHTML = "2x Cash";
+        cashCount = cashCount - 5000;
+        cashAmount.innerHTML = cashCount;
+        purchaseError.style.display = "none";
+    }
+    else if(twoTimesCash){
+        alreadyOwn.style.display = "block";
+    }
+    else{
+        purchaseError.style.display = "block";
+        currentCashAmount.innerHTML = cashCount;
     }
 }
 
